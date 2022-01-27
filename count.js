0a8d7e4ef28701c257d3bd212539af9576adec77
alicia <garlic1124@gmail.com>
2022-01-27 17:04:48 +0800

    定義資料

--- count.js
+++ count.js
@@ -1,10 +1,22 @@
 import { createApp} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.9/vue.esm-browser.min.js";
 
+const data = {
+    originPrice: 270,
+    groupPrice:210,
+    handleFee:20,
+    tktNum:1,
+    couponNum:0,
+    // originPriceTotal: tktNum * originPrice,
+    // groupPriceTotal: tktNum * groupPrice,
+    // handleFeeTotal: tktNum * handleFee,
+
+};
+
 
 const app = {
     data(){
      return {
-            name:"小明",
+            ...data,
 
         }
     },
