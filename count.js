b14920a00d5065b8e22e52c4a2bb57f8b5b2d187
alicia <garlic1124@gmail.com>
2022-01-27 16:37:30 +0800

    first commit

---
+++ count.js
@@ -0,0 +1,19 @@
+import { createApp} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.9/vue.esm-browser.min.js";
+
+
+const app = {
+    data(){
+     return {
+            name:"小明",
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+};
+
+createApp(app).mount("#app");
